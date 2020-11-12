c571014df95e765b3bc7e64cc70cfa77fa03f80f
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2020-11-12 17:41:36 -0300

    primeiro commit

---
+++ scripts/CalcController/CalController.js
@@ -0,0 +1,60 @@
+class CalController {
+
+    constructor() {
+        this._locale = 'pt-BR';
+        this._displayCalc = document.querySelector('#display');
+        this._displayDate = document.querySelector('#data');
+        this._diplayTime = document.querySelector('#hora');
+        this._currentDate;
+        this.initialize();
+    }
+
+    get displayCal() {
+        return this._displayCalc.innerHTML;
+    }
+
+    set displayCal(value) {
+        this._displayCalc.innerHTML = value;
+    }
+
+
+    get displayDate() {
+        return this._displayDate.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._displayDate.innerHTML = value;
+    }
+
+    get diplayTime() {
+        return this._diplayTime.innerHTML;
+    }
+
+    set diplayTime(value) {
+        this._diplayTime.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
+    }
+
+    initialize() {
+            this.setDisplayDateTime();
+        setInterval(()=>{
+            this.setDisplayDateTime();
+        }, 1000);
+    }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+        });
+        this.diplayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+}
