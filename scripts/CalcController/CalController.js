3163a2f1757f40ae453429ae768f05706c1be8f3
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2020-11-12 20:10:37 -0300

    tratamento de eventos

--- scripts/CalcController/CalController.js
+++ scripts/CalcController/CalController.js
@@ -63,10 +63,18 @@ class CalController {
         let buttons = document.querySelectorAll('#buttons > g, #parts > g')
 
         buttons.forEach((btn, index) =>{
-            btn.addEventListener('click', e => {
+            this.addEventListenerAll(btn, 'click drag',e => {
                 console.log(btn.className.baseVal.replace('btn-', ''));
             });
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+                btn.style.cursor = 'pointer';
+            });
         });
+    }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
     }
 }
